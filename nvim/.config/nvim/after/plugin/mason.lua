27bfff683249b68415eaa4cb6f924951161e60b8
asif <asifarbaj@gmail.com>
2025-05-07 02:53:11 +0530

    added pyright to mason ensure_installed list for python lsp support

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -1,12 +1,12 @@
 local custom_servers = require("config.lsp_servers")
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	ensure_installed = {'lua_ls', 'rust_analyzer'},
-    automatic_installation = true,
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "pyright", "lua_ls", "rust_analyzer" },
+	automatic_installation = true,
 	handlers = vim.tbl_extend("force", {
-        function(server_name)
-            require("lspconfig")[server_name].setup({})
-        end
-    }, custom_servers)
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+	}, custom_servers),
 })
