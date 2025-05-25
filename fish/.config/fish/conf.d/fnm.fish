6ff1da83efeb9f64afe6623398b00c9d96d8497b
asif <asifarbaj@gmail.com>
2025-05-26 01:57:34 +0530

    removed extra fnm functions

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -5,17 +5,3 @@ if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env | source
 end
-
-# fnm
-set FNM_PATH "/home/asif/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-end
-
-# fnm
-set FNM_PATH "/home/asif/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-end
