23303470ca8dbd2494d7c40481db14704c7ee424
asif <asifarbaj@gmail.com>
2025-07-04 21:44:42 +0530

    changed the yank highlight to Visual and timeout to 300

--- nvim/.config/nvim/after/plugin/yank-highlight.lua
+++ nvim/.config/nvim/after/plugin/yank-highlight.lua
@@ -1,7 +1,6 @@
-vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#a3c0f2", fg = "#000000" })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 200 })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 300 })
+	end,
 })
