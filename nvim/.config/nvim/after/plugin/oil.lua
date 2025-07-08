8992344e12f9a94fa5b545687b46f4f36e3ba2d7
asif <asifarbaj@gmail.com>
2025-07-09 01:38:28 +0530

    changed oil configuration to view hidden files

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,4 +1,18 @@
 require("oil").setup({
 	default_file_explorer = true,
+	view_options = {
+		show_hidden = true,
+		is_hidden_file = function(name, _)
+			return vim.startswith(name, ".")
+		end,
+		is_always_hidden = function(_, _)
+			return false
+		end,
+		natural_order = false,
+		sort = {
+			{ "type", "asc" },
+			{ "name", "asc" },
+		},
+	},
 })
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "open parent directory" })
