4a8851daf0b480158ddfebff09033048b89c95a1
asif <asifarbaj@gmail.com>
2025-07-11 00:01:36 +0530

    added virtual text for diagnostics

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -15,6 +15,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+vim.diagnostic.config({
+	virtual_text = {
+		spacing = 4,
+		source = "if_many",
+		severity = nil,
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
+})
+
+local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 local servers = {
