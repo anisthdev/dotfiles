7cd57c50c848a6816ae17c36679c01353557ba38
asif <asifarbaj@gmail.com>
2025-07-20 02:06:23 +0530

    modified tms function to take paths before asking it interactively

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -77,9 +77,14 @@ end
 
 # tmux session based on workspace directory
 function tms -d "Tmux sessionizer"
-    set -l dir (fd --type d --hidden --follow --exclude .git | fzf --prompt="Select directory: ")
-    if test -n "$dir"
-        set -l session_name (basename "$dir" | tr . _)
+    set -l dir
+    if count $argv > 0
+        set dir $argv[1]
+    else
+        set dir (fd --type d --hidden --follow --exclude .git | fzf --prompt="Select directory: ")
+    end
+    if test -n "$dir" && test -d "$dir"
+        set -l session_name (basename (realpath "$dir") | tr . _)
         if not tmux has-session -t "$session_name" 2> /dev/null
             tmux new-session -d -s "$session_name" -c "$dir"
         end
