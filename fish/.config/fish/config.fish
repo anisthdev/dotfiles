5f2aa310d41b2ab1747692723f2b346834a99b76
asif <asifarbaj@gmail.com>
2025-07-20 02:18:43 +0530

    added pk to interactively kill processes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -96,13 +96,21 @@ function tms -d "Tmux sessionizer"
     end
 end
 
-
 function gf -d "Fuzzy-find git log and return commit hash"
     set -l commit (git log --color=always --pretty=format:'%C(yellow)%h %C(reset)%s %C(bold blue)<%an>%C(reset) %C(green)(%cr)%C(reset)' |
         fzf --ansi --no-sort --reverse --tiebreak=index --preview 'git show --color=always {1}' --bind 'ctrl-y:execute-silent(echo -n {1} | tmux load-buffer -)+abort')
 
     if test -n "$commit"
         echo "$commit" | awk '{print $1}'
+    end
+end
+
+function pk -d "Fuzzy-find and kill a running process"
+    set -l pids (ps -ef | sed 1d | fzf -m --preview 'echo {}' --preview-window down:3:wrap | awk '{print $2}')
+
+    if test -n "$pids"
+        echo $pids | xargs kill -9
+        echo "Killed PID(s): $pids"
     end
 end
 
