22a4c315968d967429783df15428b75db2bd3041
asif <asifarbaj@gmail.com>
2025-07-21 04:15:38 +0530

    changed fish completion for wish to use sesshin

--- fish/.config/fish/completions/wish.fish
+++ fish/.config/fish/completions/wish.fish
@@ -1,1 +1,1 @@
-complete -c wish -n '__fish_use_subcommand' -a '(yq -r ".[] | select(has(\"ssh\")) |.name" "$SESSHIN_DIR/servers.yml" | fzf --preview "view_server {} | bat -l yml --color always -p")' -f
+complete -c wish -n '__fish_use_subcommand' -a '(sesshin list --ssh | fzf --preview "sesshin describe {} | bat -l yml --color always -p")' -f
