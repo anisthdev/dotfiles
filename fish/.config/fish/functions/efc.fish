79a5c613e9779f814f3dbbd5b2a3ce27e9e44e12
asif <asifarbaj@gmail.com>
2025-07-22 00:39:28 +0530

    renamed edit_first_command to efc

--- fish/.config/fish/functions/efc.fish
+++ fish/.config/fish/functions/efc.fish
@@ -1,5 +1,5 @@
 # edit the main command to use the remaining path
-function edit_first_command -d "Delete first command taking cursor to start of the line"
+function efc -d "Edit first command taking cursor to start of the line"
     set -l curr_command (string join " " (commandline -o | string split " " | tail -n +2))
     commandline " $curr_command"
     commandline --cursor 0
