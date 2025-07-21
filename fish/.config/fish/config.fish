32bfd6f1aae8745c1c015632bec0eed1853fc441
asif <asifarbaj@gmail.com>
2025-07-22 00:38:50 +0530

    enabled fish greeting to show random color scripts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,7 @@ starship init fish | source
 atuin init fish --disable-up-arrow | source
 fzf --fish | source
 
-# initiate fzf
+# configure fzf
 set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set -gx FZF_CTRL_T_COMMAND 'fd --type f --hidden --follow --exclude .git'
 set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --follow --exclude .git'
@@ -19,7 +19,4 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 # map keybindings
-bind \cg edit_first_command
-
-# disable fish greeting
-set -g fish_greeting
+bind \cg efc
