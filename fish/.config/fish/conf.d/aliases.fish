651946ce74d1d99d5edb005900bb13141dfbf995
asif <asifarbaj@gmail.com>
2025-07-22 00:05:41 +0530

    migrated all aliases to their own file in conf.d/aliases.fish

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,30 @@
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+alias t='tmux'
+alias ta='tmux attach || tmux new'
+alias tl='tmux list-sessions'
+
+alias gs='git status --short'
+alias ga='git add'
+alias gl='git log --oneline --graph --all'
+alias gd='git diff'
+alias gc='git commit -m'
+alias gp='git push'
+alias gpl='git pull'
+alias gsw='git switch'
+
+alias ls='eza --icons --group-directories-first'
+alias ll='eza -l --icons --group-directories-first'
+alias lt='eza -T --icons'
+
+alias cat='bat --style=auto'
+alias less='bat --style=auto --paging=always'
+
+alias c='clear'
+alias h='history'
+alias df='df -h'
+alias du='du -h'
+alias mkdir='mkdir -p'
+
