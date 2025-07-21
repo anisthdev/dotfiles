ea1480ff0c2bb07a8c42842c5edb05f8939ed587
asif <asifarbaj@gmail.com>
2025-07-22 01:23:24 +0530

    fixed the bug which crashed the function when there was a flag in the command

--- fish/.config/fish/functions/efc.fish
+++ fish/.config/fish/functions/efc.fish
@@ -1,6 +1,8 @@
 # edit the main command to use the remaining path
 function efc -d "Edit first command taking cursor to start of the line"
-    set -l curr_command (string join " " (commandline -o | string split " " | tail -n +2))
-    commandline " $curr_command"
+    set -l tokens (commandline -xp)
+    set -l new_cmd (string join -- " " $tokens[2..])
+    commandline --replace -- " $new_cmd"
     commandline --cursor 0
 end
+
