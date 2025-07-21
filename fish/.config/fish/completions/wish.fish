16339e7b029c55a4a2c074fe1eb0d898b6573636
asif <asifarbaj@gmail.com>
2025-07-22 00:07:35 +0530

    added completion for the fish command using fish complete

--- fish/.config/fish/completions/wish.fish
+++ fish/.config/fish/completions/wish.fish
@@ -1,1 +1,5 @@
-complete -c wish -n '__fish_use_subcommand' -a '(sesshin list --ssh | fzf --preview "sesshin describe {} | bat -l yml --color always -p")' -f
+# complete -c wish -n '__fish_use_subcommand' -a '(sesshin list --ssh | fzf --preview "sesshin describe {} | bat -l yml --color always -p")' -f
+yq -r '.[] | "\(.name) \(.instance)"' "$SESSHIN_DIR/servers.yml" | while read -l name instance;
+    complete -c wish -n '__fish_use_subcommand' -a "$instance" -d "$name" -f
+end
+
