54f4fc0832fb89e5d3a5f581cb252f598acae6ef
asif <asifarbaj@gmail.com>
2025-07-31 02:06:34 +0530

    set android env variables and path to sdk components

---
+++ fish/.config/fish/conf.d/development.fish
@@ -0,0 +1,5 @@
+set ANDROID_SDK_HOME $HOME/.sdks/android
+set ANDROID_AVD_HOME $ANDROID_SDK_HOME/avd
+set PATH $PATH:$ANDROID_SDK_HOME/cmdline-tools/latest/bin
+set PATH $PATH:$ANDROID_SDK_HOME/platform-tools
+set PATH $PATH:$ANDROID_SDK_HOME/emulator
