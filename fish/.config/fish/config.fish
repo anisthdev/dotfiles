2615b127cda547c8583fc5eeebb8a07c06dec65a
asif <asifarbaj@gmail.com>
2025-07-31 02:02:18 +0530

    added flutter to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 set -x PATH $PATH:$HOME/.local/bin
+set -x PATH $PATH:$HOME/.sdks/flutter/bin
 set -x TMUX_CONF $HOME/.config/tmux/.tmux.conf
 set -x TNS_ADMIN /opt/instantclient/network/admin
 set -x SESSHIN_DIR $HOME/.config/sesshin
