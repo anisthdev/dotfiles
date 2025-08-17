8e63f50e8684acf030f0c72a24e6ab44ae3d75e6
asif <asifarbaj@gmail.com>
2025-08-18 00:26:16 +0530

    added autocmd in oil to refactor code with file renames using Snacks.rename

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -16,3 +16,11 @@ require("oil").setup({
 	},
 })
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "open parent directory" })
+vim.api.nvim_create_autocmd("User", {
+	pattern = "OilActionsPost",
+	callback = function(event)
+		if event.data.actions.type == "move" then
+			Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+		end
+	end,
+})
