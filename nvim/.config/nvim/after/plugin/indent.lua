1f34b3c68b73496f2b617488f482e0919fd43e28
asif <asifarbaj@gmail.com>
2025-08-18 00:16:47 +0530

    removed indent plugin that will be replaced with snacks

--- nvim/.config/nvim/after/plugin/indent.lua
+++ nvim/.config/nvim/after/plugin/indent.lua
@@ -1,24 +1,24 @@
-require("ibl").setup({
-	indent = {
-		char = "┊", --"│",
-		tab_char = "┊",
-	},
-	scope = {
-		show_start = false,
-		show_end = false,
-	},
-	exclude = {
-		filetypes = {
-			"help",
-			"alpha",
-			"dashboard",
-			"neo-tree",
-			"Trouble",
-			"lazy",
-			"mason",
-			"notify",
-			"togglerterm",
-			"lazyterm",
-		},
-	},
-})
+-- require("ibl").setup({
+-- 	indent = {
+-- 		char = "│", --"┊",
+-- 		tab_char = "│",
+-- 	},
+-- 	scope = {
+-- 		show_start = false,
+-- 		show_end = false,
+-- 	},
+-- 	exclude = {
+-- 		filetypes = {
+-- 			"help",
+-- 			"alpha",
+-- 			"dashboard",
+-- 			"neo-tree",
+-- 			"Trouble",
+-- 			"lazy",
+-- 			"mason",
+-- 			"notify",
+-- 			"togglerterm",
+-- 			"lazyterm",
+-- 		},
+-- 	},
+-- })
