53a584542412fef83393e9431924745b14d8f9f9
asif <asifarbaj@gmail.com>
2025-08-18 00:14:50 +0530

    added lsp icons, rounded borders and changed keymaps for blink.cmp

--- nvim/.config/nvim/after/plugin/blink.lua
+++ nvim/.config/nvim/after/plugin/blink.lua
@@ -1,6 +1,6 @@
 require("blink.cmp").setup({
 	keymap = {
-		preset = "default",
+		preset = "super-tab",
 	},
 
 	appearance = {
@@ -8,7 +8,33 @@ require("blink.cmp").setup({
 	},
 
 	completion = {
-		documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "rounded" } },
+		menu = {
+			border = "rounded",
+			draw = {
+				components = {
+					kind_icon = {
+						text = function(ctx)
+							local icon = ctx.kind_icon
+							if vim.tbl_contains({ "Path" }, ctx.source_name) then
+								local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+								if dev_icon then
+									icon = dev_icon
+								end
+							else
+								icon = require("lspkind").symbolic(ctx.kind, {
+									mode = "symbol",
+								})
+							end
+
+							return icon .. ctx.icon_gap
+						end,
+					},
+				},
+			},
+		},
+		trigger = { show_in_snippet = false },
+		signature = { window = { border = "rounded" } },
 	},
 
 	sources = {
@@ -26,4 +52,3 @@ require("blink.cmp").setup({
 	fuzzy = { implementation = "lua" },
 	signature = { enabled = true },
 })
-
