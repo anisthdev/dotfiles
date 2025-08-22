9c85bc55ea09610af3c73421ee48dd8734b83dfb
asif <asifarbaj@gmail.com>
2025-08-22 21:05:11 +0530

    setup lspsaga config

--- nvim/.config/nvim/after/plugin/lspsaga.lua
+++ nvim/.config/nvim/after/plugin/lspsaga.lua
@@ -1,1 +1,14 @@
-require("lspsaga").setup({})
+require("lspsaga").setup({
+	ui = {
+		border = "rounded",
+		devicon = true,
+		code_action = " ",
+		title = false,
+	},
+	lightbulb = {
+		enable = false,
+	},
+	symbol_in_winbar = {
+		enable = false,
+	},
+})
