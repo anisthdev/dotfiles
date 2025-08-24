badaec7d0c06e73a3a6f0f76992179bb745a21cb
asif <asifarbaj@gmail.com>
2025-08-25 01:51:34 +0530

    removed dad bod completions

--- nvim/.config/nvim/after/plugin/blink.lua
+++ nvim/.config/nvim/after/plugin/blink.lua
@@ -38,9 +38,6 @@ require("blink.cmp").setup({
 
 	sources = {
 		default = { "lsp", "path", "snippets", "lazydev" },
-		per_filetype = {
-			sql = { "snippets", "dadbod", "buffer" },
-		},
 		providers = {
 			lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
