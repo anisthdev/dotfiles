599d34ef16168c1297c67c1bebca4f0451f3aa5c
asif <asifarbaj@gmail.com>
2025-08-27 20:47:03 +0530

    updated flutter-tools to show colors as virtualtext

--- nvim/.config/nvim/after/plugin/flutter.lua
+++ nvim/.config/nvim/after/plugin/flutter.lua
@@ -1,12 +1,12 @@
 require("flutter-tools").setup({
 	lsp = {
-		color = { -- show the derived colours for dart variables
-			enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-			background = false, -- highlight the background
-			background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-			foreground = false, -- highlight the foreground
-			virtual_text = true, -- show the highlight using virtual text
-			virtual_text_str = "■", -- the virtual text character to highlight
+		color = {
+			enabled = true,
+			background = false,
+			background_color = nil,
+			foreground = false,
+			virtual_text = true,
+			virtual_text_str = "󱓻",
 		},
 		on_attach = function(_, bufnr)
 			local opts = { buffer = bufnr, desc = "" }
