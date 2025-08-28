2a273d08081af52956e5113afe2ad7508c4dd256
asif <asifarbaj@gmail.com>
2025-08-29 01:34:48 +0530

    added ghost text and lspkind icons to blink

--- nvim/.config/nvim/after/plugin/blink.lua
+++ nvim/.config/nvim/after/plugin/blink.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_set_hl(0, "Pmenu", { link = "NormalFloat" })
+require("lspkind").init({
+	mode = "symbol_text",
+	preset = "codicons",
+})
+
 require("blink.cmp").setup({
 	keymap = {
 		preset = "default",
@@ -8,26 +14,16 @@ require("blink.cmp").setup({
 	},
 
 	completion = {
+		ghost_text = { enabled = true, show_with_menu = true },
 		documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "rounded" } },
 		menu = {
+			auto_show = true,
 			border = "rounded",
 			draw = {
 				components = {
 					kind_icon = {
 						text = function(ctx)
-							local icon = ctx.kind_icon
-							if vim.tbl_contains({ "Path" }, ctx.source_name) then
-								local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-								if dev_icon then
-									icon = dev_icon
-								end
-							else
-								icon = require("lspkind").symbolic(ctx.kind, {
-									mode = "symbol",
-								})
-							end
-
-							return icon .. ctx.icon_gap
+							return " " .. ctx.kind_icon .. ctx.icon_gap .. " "
 						end,
 					},
 				},
@@ -46,5 +42,5 @@ require("blink.cmp").setup({
 
 	snippets = { preset = "luasnip" },
 	fuzzy = { implementation = "lua" },
-	signature = { enabled = true },
+	signature = { enabled = true, window = { border = "rounded" } },
 })
