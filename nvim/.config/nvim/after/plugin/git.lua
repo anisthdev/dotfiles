458f58254ab27791b8655211077636130517d286
asif <asifarbaj@gmail.com>
2025-08-28 23:28:48 +0530

    unified the git signs and fugitive configs

--- nvim/.config/nvim/after/plugin/git.lua
+++ nvim/.config/nvim/after/plugin/git.lua
@@ -1,6 +1,4 @@
--- ~/.config/nvim/lua/plugins/gitsigns.lua
-
-require("gitsigns").setup({
+require("after.plugin.git").setup({
 	signs = {
 		add = { text = "│" },
 		change = { text = "│" },
@@ -39,7 +37,7 @@ require("gitsigns").setup({
 
 	-- Keymaps
 	on_attach = function(bufnr)
-		local gs = require("gitsigns")
+		local gs = require("after.plugin.git")
 
 		local function map(mode, l, r, opts)
 			opts = opts or {}
@@ -69,22 +67,62 @@ require("gitsigns").setup({
 		end, { expr = true, desc = "Go to previous git hunk" })
 
 		-- Actions
-		map({ "n", "v" }, "<leader>hs", gs.stage_hunk, { desc = "Stage git hunk" })
-		map({ "n", "v" }, "<leader>hr", gs.reset_hunk, { desc = "Reset git hunk" })
-		map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage entire buffer" })
-		map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo last staged hunk" })
-		map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset entire buffer" })
-		map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview git hunk" })
-		map("n", "<leader>hb", function()
+		map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage git hunk" })
+		map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset git hunk" })
+		map("n", "<leader>gS", gs.stage_buffer, { desc = "Stage entire buffer" })
+		map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo last staged hunk" })
+		map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset entire buffer" })
+		map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview git hunk" })
+		map("n", "<leader>gb", function()
 			gs.blame_line({ full = true })
 		end, { desc = "Git blame line" })
-		map("n", "<leader>htb", gs.toggle_current_line_blame, { desc = "Toggle line blame" })
-		map("n", "<leader>hd", gs.diffthis, { desc = "Diff against index" })
-		map("n", "<leader>hD", function()
+		map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Toggle line blame" })
+		map("n", "<leader>gd", gs.diffthis, { desc = "Diff against index" })
+		map("n", "<leader>gD", function()
 			gs.diffthis("~1")
 		end, { desc = "Diff against last commit" })
 
 		-- Text object
 		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select git hunk" })
 	end,
+})
+
+local map = vim.keymap.set
+
+map("n", "<leader>gg", ":Git<cr>", { desc = "Git status" })
+map("n", "<leader>gc", ":Git commit<cr>", { desc = "Git commit" })
+map("n", "<leader>gP", ":Git push<cr>", { desc = "Git push" })
+map("n", "<leader>gl", ":Git log --oneline --graph<cr>", { desc = "Git diff in split" })
+map("n", "<leader>gh", ":0Gclog<cr>", { desc = "Git history for current file" })
+
+vim.api.nvim_create_augroup("GitLayout", { clear = true })
+
+local function open_bottom()
+	vim.cmd("wincmd J")
+	vim.cmd("resize 15")
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = "GitLayout",
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+		if ft == "fugitive" then
+			vim.cmd("wincmd H")
+			local total_cols = vim.o.columns
+			local target_width = math.floor(total_cols / 4)
+			vim.cmd("vertical resize " .. target_width)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = "GitLayout",
+	pattern = "COMMIT_EDITMSG",
+	callback = open_bottom,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = "GitLayout",
+	pattern = "git",
+	callback = open_bottom,
 })
