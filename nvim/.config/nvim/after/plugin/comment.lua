88a3527ad841fea11a5173b399d1742a49ceb913
asif <asifarbaj@gmail.com>
2025-08-29 20:29:36 +0530

    changed keybinding of comments

--- nvim/.config/nvim/after/plugin/comment.lua
+++ nvim/.config/nvim/after/plugin/comment.lua
@@ -2,11 +2,11 @@ require("Comment").setup({
 	-- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 	toggler = {
 		line = "<leader>cc", -- Line-comment toggle keymap
-		block = "<leader>bc",
+		block = "<leader>cb",
 	},
 	opleader = {
 		line = "<leader>c",
-		block = "<leader>b",
+		block = "<leader>C",
 	},
 	extra = {
 		above = "<leader>cO",
