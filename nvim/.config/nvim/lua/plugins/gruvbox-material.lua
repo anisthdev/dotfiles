976745ce1bd9697c840420a0a578d25e9d437878
asif <asifarbaj@gmail.com>
2025-09-09 00:59:34 +0530

    removed custom highlights and turned on colorscheme's default features.

--- nvim/.config/nvim/lua/plugins/gruvbox-material.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox-material.lua
@@ -2,45 +2,12 @@ return {
 	"sainnhe/gruvbox-material",
 	priroty = 1000,
 	config = function()
-		vim.cmd("colorscheme gruvbox-material")
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
-			fg = "#ea6962", -- red
-			bg = "#3c3836", -- dark bg
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", {
-			fg = "#d8a657", -- yellow
-			bg = "#3c3836",
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", {
-			fg = "#7daea3", -- aqua
-			bg = "#3c3836",
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", {
-			fg = "#a9b665", -- green
-			italic = true,
-		})
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesError", {
-			fg = "#ea6962", -- red
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesWarn", {
-			fg = "#d8a657", -- yellow
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesInfo", {
-			fg = "#7daea3", -- aqua
-		})
-
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesHint", {
-			fg = "#a9b665", -- green
-			italic = true,
-		})
+		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_background = "soft"
+		vim.g.gruvbox_material_float_style = "dim"
+		vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
+		vim.g.gruvbox_material_diagnostic_line_highlight = 1
+		vim.g.gruvbox_material_menu_selection_background = "blue"
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
