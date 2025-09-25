5a501651b1d93c1c7d363bca356e1c06bd62e7b0
asif <asifarbaj@gmail.com>
2025-09-26 01:06:50 +0530

    refactored blink config

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,27 +1,28 @@
 return {
 	"saghen/blink.cmp",
-	event = "VimEnter",
-	version = "1.*",
+	event = "VeryLazy",
 	dependencies = {
-		{ "L3MON4D3/LuaSnip", version = "2.*", build = "make install_jsregexp" },
+		{ "L3MON4D3/LuaSnip", keys = {} },
 		"rafamadriz/friendly-snippets",
 	},
 	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
-
 		require("blink.cmp").setup({
 			completion = {
-				menu = { auto_show = false, border = "none" },
+				menu = {
+					auto_show = false,
+					border = "none",
+					draw = { columns = { { "kind_icon" }, { "label" }, { "kind" }, { "source_name" } } },
+				},
 				ghost_text = { enabled = true, show_with_menu = false },
 				documentation = { auto_show = true, auto_show_delay_ms = 1500 },
 				trigger = { show_in_snippet = false },
 			},
-			sources = {
-				default = { "lsp", "path", "snippets", "lazydev" },
-				providers = { lazydev = { module = "lazydev.integrations.blink", score_offset = 100 } },
-			},
+			sources = { default = { "lsp", "path", "snippets", "buffer" } },
+			appearance = { use_nvim_cmp_as_default = false },
 			snippets = { preset = "luasnip" },
 			fuzzy = { implementation = "lua" },
 		})
+
+		require("luasnip.loaders.from_vscode").lazy_load()
 	end,
 }
