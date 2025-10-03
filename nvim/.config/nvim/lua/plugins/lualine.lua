19bc7f6737108731332bc5634c61124f7a36bcfc
asif <asifarbaj@gmail.com>
2025-10-03 12:57:36 +0530

    added sidekick statusline component

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -101,6 +101,23 @@ return {
 					},
 					"diff",
 					"diagnostics",
+					{
+						function()
+							return " "
+						end,
+						color = function()
+							local status = require("sidekick.status").get()
+							if status then
+								return status.kind == "Error" and "DiagnosticError"
+									or status.busy and "DiagnosticHint"
+									or "String"
+							end
+						end,
+						cond = function()
+							local status = require("sidekick.status")
+							return status.get() ~= nil
+						end,
+					},
 				},
 				lualine_y = {
 					{
